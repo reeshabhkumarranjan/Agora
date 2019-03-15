c8bc3969e262a4f597ba933b88c24efb79aa85c1
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2019-03-15 23:43:43 +0530

    Splash-screen opens correctly now.

--- app/src/main/java/com/example/agora/SplashScreen.java
+++ app/src/main/java/com/example/agora/SplashScreen.java
@@ -1,5 +1,7 @@
 package com.example.agora;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        int delayDuration=2000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },delayDuration);
     }
 }
