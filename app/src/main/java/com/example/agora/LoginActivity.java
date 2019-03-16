42a0bbeead36be9fce563507c70310d717a9e9a3
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2019-03-16 17:24:53 +0530

    Now image view displays the avatar inside a circle. Also started some intents on tapping a few buttons (for testing).

--- app/src/main/java/com/example/agora/LoginActivity.java
+++ app/src/main/java/com/example/agora/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.agora;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,8 +12,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-
-
+        Button signInButton=findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,Dashboard.class);
+                startActivity(intent);
+            }
+        });
     }
 }
