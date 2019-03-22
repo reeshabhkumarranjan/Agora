f771c2bdbf09e7963b2b27e3c1d427cdce7d48ef
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2019-03-22 18:43:13 +0530

    Added description section for election details.

--- app/src/main/java/com/example/agora/view/Dashboard.java
+++ app/src/main/java/com/example/agora/view/Dashboard.java
@@ -37,8 +37,8 @@ public class Dashboard extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent=new Intent(Dashboard.this,Vote.class);
+                startActivity(intent);
             }
         });
 
---
+++ app/src/main/java/com/example/agora/view/Vote.java
@@ -0,0 +1,31 @@
+package com.example.agora.view;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.agora.R;
+
+public class Vote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vote);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
