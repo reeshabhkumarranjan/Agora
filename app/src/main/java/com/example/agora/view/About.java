da6546cf6771958f30a36c983d0f81fdb31803d4
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2019-04-04 02:27:35 +0530

    Added url to button in about screen.

--- app/src/main/java/com/example/agora/view/About.java
+++ app/src/main/java/com/example/agora/view/About.java
@@ -1,5 +1,7 @@
 package com.example.agora.view;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.agora.R;
@@ -38,8 +41,21 @@ public class About extends AppCompatActivity {
         creditArrayList.add("Smashicons");
 
         // populating credits list
+        // thanks to https://stackoverflow.com/a/4540859/5394180
         ListView creditsListView=(ListView)findViewById(R.id.creditsListView);
         ArrayAdapter<String> stringArrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,creditArrayList);
         creditsListView.setAdapter(stringArrayAdapter);
+
+        // opening link on taps
+        Button learnMoreButton=(Button)findViewById(R.id.learnMoreButton);
+        learnMoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // thanks to https://stackoverflow.com/a/4930319/5394180
+                Uri uri = Uri.parse("https://gitlab.com/aossie/Agora/");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
     }
 }
